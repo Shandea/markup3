bd349163b8cbded05f9ef107238fcb5b456b0a84
Shandea <86807815+Shandea@users.noreply.github.com>
2021-07-20 14:05:32 -0700

    service links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
-import logo from "./logo.svg";
 import "./App.css";
+import image1 from "./images/image1.jpg";
+import image2 from "./images/image2.jpg";
+import image3 from "./images/image3.jpg";
 
 function App() {
   return (
     <div className="App">
-      <h1>Markup Page</h1>
+      <div class="row">
+        <div class="col s12">
+          <nav id="navBar">
+            <div class="nav-wrapper">
+              <a href="#servicePage" class="brand-logo">
+                &#9875;Markup Page&#9875;
+              </a>
+            </div>
+          </nav>
+        </div>
+      </div>
+      <div className="container">
+        <div className="item">
+          {" "}
+          <a href="#servicePage">Home</a>
+          <img src={image1} alt="picture1" />
+          Tech Support
+        </div>
+        <div className="item">
+          {" "}
+          <a href="#servicePage">Home</a>
+          <img src={image2} alt="picture2" />
+          Career Paths
+        </div>
+        <div className="item">
+          {" "}
+          <a href="#servicePage">Home</a>
+          <img src={image3} alt="picture3" />
+          Website Development
+        </div>
+      </div>
     </div>
   );
 }
