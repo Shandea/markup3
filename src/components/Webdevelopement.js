de6a34f4943b25b6efef786f33fecf4872bb607b
Shandea <86807815+Shandea@users.noreply.github.com>
2021-07-26 14:43:26 -0700

    form submit functional

--- src/components/Webdevelopement.js
+++ src/components/Webdevelopement.js
@@ -1,19 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import emailjs from 'emailjs-com';
 
-export default function Webdevelopement() {
+
+const Webdevelopement = () => {
+  const [user, setUser] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    subject:""
+  });
+  const onChangeUser = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_fok7ni4', 'template_oic68dn', e.target, 'user_mpjYJ5MjFKGH3OwW3i9GU').then(res => {
+        console.log(res)
+    }).catch(err => {
+        console.log(err)
+    })
+  };
   return (
     <div className="webBody">
       <h1 className="textStyling">Website Developement Page </h1>
-      <form className="contactform">
-      <div className="inputArea">
-        <h3 className="textStyling">Please contact us for a free quote</h3>
+      <form className="contactform" onSubmit={onSubmit}>
+        <div className="inputArea">
+          <h3 className="textStyling">Please contact us for a free quote</h3>
           <label for="name">
-            <input className="firstName" type="text" placeholder="First Name" />
-            <input className="lastName" type="text" placeholder="Last Name" />
+            <input
+              className="firstName"
+              type="text"
+              value={user.firstname}
+              name="firstname"
+              placeholder="First Name"
+              onChange={onChangeUser}
+            />
+            <input
+              className="lastName"
+              type="text"
+              value={user.lastname}
+              name="lastname"
+              placeholder="Last Name"
+              onChange={onChangeUser}
+            />
           </label>
           <br />
           <label for="email">
-            <input className="email" type="text" placeholder="Email" />
+            <input
+              className="email"
+              type="text"
+              value={user.email}
+              name="email"
+              placeholder="Email"
+              onChange={onChangeUser}
+            />
           </label>
           <br />
           <button className="submit" type="submit">
@@ -23,4 +65,6 @@ export default function Webdevelopement() {
       </form>
     </div>
   );
-}
+};
+
+export default Webdevelopement;
